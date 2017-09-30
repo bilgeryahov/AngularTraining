8c2dc15caa38dcbcffc0750050a04db49ab9822d
Bilger Yahov <bilger@studyportals.com>
2017-09-30 19:07:15 +0200

    Add a new component - ServerComponent.

--- my-first-angular-app/src/app/app.module.ts
+++ my-first-angular-app/src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from "@angular/forms";
 
+// Components.
 import { AppComponent } from './app.component';
-import {FormsModule} from "@angular/forms";
+import { ServerComponent } from './server/server.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent
   ],
   imports: [
     BrowserModule,
@@ -15,4 +18,5 @@ import {FormsModule} from "@angular/forms";
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
