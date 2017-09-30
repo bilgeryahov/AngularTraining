adffc13ca74216abc5f87efed9d51720f451bae2
Bilger Yahov <bilger@studyportals.com>
2017-09-30 19:43:04 +0200

    Add warning message component.

--- my-first-angular-app/src/app/app.module.ts
+++ my-first-angular-app/src/app/app.module.ts
@@ -6,14 +6,16 @@ import { FormsModule } from "@angular/forms";
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component'
-import { SuccessMessageComponent } from './successMessage/successMessage.component';
+import { SuccessMessageComponent } from './success-message/success-message.component';
+import { WarningMessageComponent } from './warning-message/warning-message.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
     ServersComponent,
-    SuccessMessageComponent
+    SuccessMessageComponent,
+    WarningMessageComponent
   ],
   imports: [
     BrowserModule,
