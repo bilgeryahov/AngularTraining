5d6ee787e6625161406db3b712a85292f7e7eeb5
Bilger Yahov <bilger@studyportals.com>
2017-09-30 19:35:09 +0200

    Add SuccessMessageComponent.

---
+++ my-first-angular-app/src/app/successMessage/successMessage.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-success-message',
+  templateUrl: './successMessage.component.html',
+  styleUrls: ['./successMessage.component.css']
+})
+
+export class SuccessMessageComponent {
+
+}
