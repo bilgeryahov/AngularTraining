9c8aebf5675dcd362a55722a3e536877b411ff94
Bilger Yahov <bilger@studyportals.com>
2017-10-01 21:44:50 +0200

    Initialize component for practicing directives.

---
+++ my-first-angular-app/src/app/practice-directives/practice-directives.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-practice-directives',
+  templateUrl: './practice-directives.component.html',
+  styleUrls: ['./practice-directives.component.css']
+})
+export class PracticeDirectivesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
