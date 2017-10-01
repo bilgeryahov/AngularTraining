b1d596d37bb1dceb722f332a81d217cade1b0045
Bilger Yahov <bilger@studyportals.com>
2017-10-01 22:22:37 +0200

    Implement the practice exercise for directives.

--- my-first-angular-app/src/app/practice-directives/practice-directives.component.ts
+++ my-first-angular-app/src/app/practice-directives/practice-directives.component.ts
@@ -6,10 +6,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./practice-directives.component.css']
 })
 export class PracticeDirectivesComponent implements OnInit {
+  detailsDisplayedDivisibleByTwo = false;
+  detailsDisplayed = 0;
+  detailsDisplayClicks = [];
 
-  constructor() { }
+  constructor() {
+    this.changeDetailsDisplayed();
+  }
 
   ngOnInit() {
   }
 
+  /**
+   * Changes the details displayed. Increases it.
+   * Calls for determining if it is divisible by two.
+   * Function gets called from inside of the class.
+   */
+
+  changeDetailsDisplayed(){
+    this.detailsDisplayed ++;
+    this.determineDetailsDisplayedDivisibleByTwo();
+  }
+
+  /**
+   * Determines if the details divisible is divisible
+   * by two.
+   * Function gets called from inside of the class.
+   */
+
+  determineDetailsDisplayedDivisibleByTwo(){
+    this.detailsDisplayedDivisibleByTwo = this.detailsDisplayed % 2 === 0;
+  }
+
+  /**
+   * Gets called from the template,
+   * whenever the button is clicked.
+   */
+
+  onDetailsDisplayed(){
+    this.detailsDisplayClicks.push(new Date());
+    this.changeDetailsDisplayed();
+  }
 }
